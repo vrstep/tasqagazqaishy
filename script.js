68f1f43c8c41b1c7bd369bb506b29dffa80d33c3
Amir <amir.batyrggaliev@gmail.com>
2022-11-24 16:29:13 +0600

    add game()

--- script.js
+++ script.js
@@ -5,31 +5,36 @@ function getComputerChoice() {
     return computerChoice[random];
 }
 
-let playerSelection = prompt("Rock, paper or scissors?").toLowerCase();
-let computerSelection = getComputerChoice(computerChoice);
-
-console.log(`Player's choice: ${playerSelection}. Computer's choice: ${computerSelection}`);
-
 function playRound(playerSelection, computerSelection) {
     switch (playerSelection) {
         case 'rock':
             if (computerSelection == 'paper') console.log("You lost");
-            else if (computerChoice == 'scissors') console.log("You won");
+            else if (computerSelection == 'scissors') console.log("You won");
             else console.log("Tie");
             break;
         
         case 'paper':
             if (computerSelection == 'scissors') console.log("You lost");
-            else if (computerChoice == 'rock') console.log("You won");
+            else if (computerSelection == 'rock') console.log("You won");
             else console.log("Tie");
             break;
 
         case 'scissors':
             if (computerSelection == 'rock') console.log("You lost");
-            else if (computerChoice == 'paper') console.log("You won");
+            else if (computerSelection == 'paper') console.log("You won");
             else console.log("Tie");
             break;
     }
 }
 
-console.log(playRound(playerSelection, computerSelection))
+function game() {
+    for (let i = 1; i <= 5; i++) {
+        let playerSelection = prompt("Rock, paper or scissors?").toLowerCase();
+        let computerSelection = getComputerChoice(computerChoice);
+        console.log(`Player's choice: ${playerSelection}. Computer's choice: ${computerSelection}`);
+        console.log(`Round ${i}`);
+        playRound(playerSelection, computerSelection);
+    }
+}
+
+console.log(game());
